bf4cbe64200b11d39f9619a84c89a5802adafe0e
YRNBAM <145809841+bigbam974@users.noreply.github.com>
2024-10-01 17:47:16 -0400

    Add files via upload

---
+++ mcdoanlds/install.lua
@@ -0,0 +1,97 @@
+this is to just getr them into the server you have to make a shop accordingly to the script your using
+
+join discords for support
+
+-------------------------------QB/QB-Core/Shared/Items-----------------------------------
+ 
+
+----McDonalds
+    bigmac 					= { name = 'bigmac', 			 	  	label = 'BigMac Burger', 	                weight = 200, 		type = 'item', 		image = 'bigmac.png', 			unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	mcchicken 				= { name = 'mcchicken', 			 	label = 'McChicken Burger', 	           	weight = 200, 		type = 'item', 		image = 'mcchicken.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	quarterpounder 				= { name = 'quarterpounder', 			 	label = 'Quarter Pounder Burger', 	        weight = 200, 		type = 'item', 		image = 'quarterpounder.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	dquarterpounder 			= { name = 'dquarterpounder', 			 	label = 'Double Quarter Pounder Burger', 	weight = 200, 		type = 'item', 		image = 'dquarterpounder.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	filet_o_fish 				= { name = 'filet-o-fish', 			 	label = 'Fillet-O-Fish Burger', 	        weight = 200, 		type = 'item', 		image = 'filet-o-fish.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	bigmac_meal				= { name = 'bigmac_meal', 			 	label = 'BigMac Meal', 	                    	weight = 200, 		type = 'item', 		image = 'bigmac_meal.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	mcchicken_meal 				= { name = 'mcchicken_meal', 			 	label = 'McChicken Meal', 	                weight = 200, 		type = 'item', 		image = 'mcchicken_meal.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	quarterpounder_meal 			= { name = 'quarterpounder_meal', 			label = 'Quarter Pounder Meal', 	        weight = 200, 		type = 'item', 		image = 'quarterpounder_meal.png', 	unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	dquarterpounder_meal 			= { name = 'dquarterpounder_meal', 			label = 'Double Quarter Pounder Meal', 	    	weight = 200, 		type = 'item', 		image = 'dquarterpounder_meal.png', 	unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	filet_o_fish_meal 			= { name = 'filet-o-fish_meal', 			label = 'Fillet-O-Fish Meal', 	            	weight = 200, 		type = 'item', 		image = 'filet-o-fish_meal.png', 	unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	cheeseburger 				= { name = 'cheeseburger', 			 	label = 'Cheese Burger', 	                weight = 200, 		type = 'item', 		image = 'cheeseburger.png', 		unique = false, 	useable = true,   shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	dcheeseburger 				= { name = 'dcheeseburger', 			 	label = 'Double Cheese Burger',     	    	weight = 200, 		type = 'item', 		image = 'dcheeseburger.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	tcheeseburger 				= { name = 'tcheeseburger', 			 	label = 'Triple Cheese Burger', 	        weight = 200, 		type = 'item', 		image = 'tcheeseburger.png', 	        unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	sixpnuggets				= { name = 'sixpnuggets', 			 	label = '6 Chicken Nuggets', 	            	weight = 200, 		type = 'item', 		image = '6pnuggets.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	tenpnuggets 				= { name = 'tenpnuggets', 			 	label = '10 Chicken Nuggets', 	            	weight = 200, 		type = 'item', 		image = '10pnuggets.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	twentypnuggets 				= { name = 'twentypnuggets', 			 	label = '20 Chicken Nuggets', 	            	weight = 200, 		type = 'item', 		image = '20pnuggets.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	cheeseburger_meal 			= { name = 'cheeseburger_meal', 			label = 'Cheeseburger Meal', 	           	weight = 200, 		type = 'item', 		image = 'cheeseburger_meal.png', 	unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	dcheeseburger_meal 			= { name = 'dcheeseburger_meal', 			label = 'Double Cheeseburger Meal', 	    	weight = 200, 		type = 'item', 		image = 'dcheeseburger_meal.png', 	unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	tcheeseburger_meal 			= { name = 'tcheeseburger_meal', 			label = 'Tripple Cheeseburger Meal', 	    	weight = 200, 		type = 'item', 		image = 'tcheeseburger_meal.png', 	unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	sixpnuggets_meal 			= { name = 'sixpnuggets_meal', 			 	label = '6 Chicken Nugget Meal', 	        weight = 200, 		type = 'item', 		image = '6pnuggets_meal.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	tenpnuggets_meal 			= { name = 'tenpnuggets_meal', 			 	label = '10 Chicken Nugget Meal', 	        weight = 200, 		type = 'item', 		image = '10pnuggets_meal.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	twentypnuggets_meal 			= { name = 'twentypnuggets_meal', 			 	label = '20 Chicken Nugget Meal', 	        weight = 200, 		type = 'item', 		image = '20pnuggets_meal.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,  		description = 'Nice to eat'},
+	sfries 					= { name = 'sfries', 			 	  	label = 'Small Fries', 	                    	weight = 200, 		type = 'item', 		image = 'sfries.png', 			unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	mfries 					= { name = 'mfries', 			 	  	label = 'Meduim Fries', 	                weight = 200, 		type = 'item', 		image = 'mfries.png', 			unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	lfries 					= { name = 'lfries', 			 	  	label = 'Large Fries', 	                    	weight = 200, 		type = 'item', 		image = 'lfries.png', 			unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	hotapplepie 				= { name = 'hotapplepie', 			 	label = 'Hot Apple Pie', 	                weight = 200, 		type = 'item', 		image = 'hotapplepie.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	ssconef 				= { name = 'ssconef', 			 	  	label = 'Soft Serve Cone with a Flake', 	weight = 200, 		type = 'item', 		image = 'ssconef.png', 			unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	sscone					= { name = 'sscone', 			 	  	label = 'Soft Serve Cone', 	                weight = 200, 		type = 'item', 		image = 'sscone.png', 			unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	mflurryo 				= { name = 'mflurryo', 			 	  	label = 'Oreo McFlurry', 	                weight = 200, 		type = 'item', 		image = 'mflurryo.png', 		unique = false, 	useable = true,   shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+	mflurrym				= { name = 'mflurrym', 			 	  	label = 'M&M McFlurry', 	                weight = 200, 		type = 'item', 		image = 'mflurrym.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,  		description = 'Nice to eat'},
+	cocacola 				= { name = 'cocacola', 			 	  	label = 'McDonalds Coca-Cola', 	            	weight = 200, 		type = 'item', 		image = 'cocacola.png', 		unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   	description = 'Nice to eat'},
+
+
+
+
+ --------------------------QB/QB-Smallresources/Config-------------------------------
+
+ ----food
+ ['bigmac'] = math.random(15, 20),
+ ['mcchicken'] = math.random(15, 20),
+ ['quarterpounder'] = math.random(15, 20),
+ ['dquarterpounder'] = math.random(15, 20),
+ ['filet-o-fish'] = math.random(15, 20),
+ ['cheeseburger'] = math.random(15, 20),
+ ['bigmac_meal'] = math.random(25, 40),
+ ['mcchicken_meal'] = math.random(25, 40),
+ ['quarterpounder_meal'] = math.random(25, 40),
+ ['dquarterpounder_meal'] = math.random(25, 40),
+ ['filet-o-fish_meal'] = math.random(25, 40),
+ ['cheeseburger_meal'] = math.random(25, 40),
+ ['dcheeseburger'] = math.random(15, 20),
+ ['tcheeseburger'] = math.random(15, 20),
+ ['6pnuggets'] = math.random(15, 20),
+ ['10pnuggets'] = math.random(15, 20),
+ ['20pnuggets'] = math.random(15, 20),
+ ['dcheeseburger_meal'] = math.random(25, 40),
+ ['tcheeseburger_meal'] = math.random(25, 40),
+ ['6pnuggets_meal'] = math.random(25, 40),
+ ['10pnuggets_meal'] = math.random(25, 40),
+ ['20pnuggets_meal'] = math.random(25, 40),
+ ['sfries'] = math.random(5, 10),
+ ['mfries'] = math.random(15, 20),
+ ['lfries'] = math.random(20, 25),
+ ['hotapplepie'] = math.random(15, 20),
+
+---drinks
+['ssconef'] = math.random(15, 20),
+['sscone'] = math.random(15, 20),
+['mflurryo'] = math.random(15, 20),
+['mflurrym'] = math.random(15, 20),
+['cocacola'] = math.random(20, 30),
+
+------------------ADD IMAGES TO QB/QB-inventory/HTML/Images--------------------------
+
+
+Enjoyyyyyyyyy
+
+___YRNBAM
+
+join my discords
+
+--Cars&Things
+https://discord.gg/h3deXeGcBC
+
+--SDRP.V1
+https://discord.gg/f5BR4AVBev
+
+--Westpalm100kordie
+https://discord.gg/f5BR4AVBev
